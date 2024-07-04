06a23bd5101620e6ffc65f4e32e58c02f60807b1
jaemin0619 <wjdwoals000619@gmail.com>
2024-07-04 17:17:03 +0900

    first time

---
+++ src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello Java1");
+        System.out.println("hello Java2");
+        System.out.println("hello Java3");
+    }
+
+}
