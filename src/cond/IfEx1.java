dc01c6ff8576846b27b9b492236ec6260da4b184
jaemin0619 <wjdwoals000619@gmail.com>
2024-07-08 21:11:19 +0900

    Third upload

---
+++ src/cond/IfEx1.java
@@ -0,0 +1,28 @@
+package cond;
+
+public class IfEx1 {
+    public static void main(String[] args) {
+        int score=70;
+        if(score>=95)
+        {
+            System.out.println("학점은 A입니다.");
+        }
+        else if(score>=85)
+        {
+            System.out.println("학점은 B입니다.");
+        }
+        else if(score>=75)
+        {
+            System.out.println("학점은 C입니다.");
+        }
+        else if(score>=65)
+        {
+            System.out.println("학점은 D입니다.");
+        }
+        else (score<60)
+        {
+            System.out.println("학점은 F입니다.");
+        }
+
+    }
+}
---
+++ src/cond/IfEx2.java
@@ -0,0 +1,8 @@
+package cond;
+
+public class IfEx2 {
+    public static void main(String[] args) {
+      double rating;
+
+    }
+}
